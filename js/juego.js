080628449b3bf35de6c48d3e4c4bf1dfecd2f6ef
bloid3 <pablo.pereira00@gmail.com>
2022-10-14 12:40:01 +0200

    Subiendo código

--- js/juego.js
+++ js/juego.js
@@ -1,3 +1,11 @@
+const buscaminas = new Map()
+buscaminas.set("Filas", 10)
+buscaminas.set("Columnas", 10)
+buscaminas.set("Minas", 50)
+
+
+
+
 function pintarTab(numC, numF) {
 	let tablero = document.querySelector("#tablero")
 	for (let f = 1; f <= numF; f++) {
@@ -6,5 +14,23 @@ function pintarTab(numC, numF) {
 			nuevoDiv.setAttribute("id","idCelda_" + f + "_" + c)
 			tablero.appendChild(nuevoDiv)
 		}
+	}
+}
+function generarCampoMinas(numC, numF) {
+	campoMinas = new Array(numF)
+	for (let fila = 0; fila < numF; fila++) {
+		campoMinas[f] = new Array(numC)
+	}
+}
+function esparcirMinas(numC, numF, minasTotales) {
+	let minasColocadas = 0
+	while (minasColocadas < minasTotales) {
+		let fila = Math.floor(Math.random() * numF)
+		let columna = Math.floor(Math.random() * numC)
+
+		if (campoMinas[fila][columna] != "B") {
+			campoMinas[fila][columna] == "B"
+		}
+		minasColocadas++;
 	}
 }
