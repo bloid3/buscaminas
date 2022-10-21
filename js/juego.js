ae76eba5aed214dd37fb9e290da7b7c5bb167410
bloid3 <pablo.pereira00@gmail.com>
2022-10-21 10:16:24 +0200

    Subiendo código

--- js/juego.js
+++ js/juego.js
@@ -1,16 +1,15 @@
-const buscaminas = new Map()
-buscaminas.set("Filas", 10)
-buscaminas.set("Columnas", 10)
-buscaminas.set("Minas", 50)
+const buscaminas = {
+	filas:10,
+	columnas:10,
+	minas:(filas*columnas/2),
+	campoMinas:[]
+}
 
-
-
-
-function pintarTab(numC, numF) {
+function pintarTab() {
 	if (!document.getElementById("idCelda_1_1")) {
 		let tablero = document.querySelector("#tablero")
-		for (let f = 1; f <= numF; f++) {
-			for (let c = 1; c <= numC; c++) {
+		for (let f = 1; f <= buscaminas.filas; f++) {
+			for (let c = 1; c <= buscaminas.columnas; c++) {
 				let nuevoDiv = document.createElement("div")
 				nuevoDiv.setAttribute("id","idCelda_" + f + "_" + c)
 				tablero.appendChild(nuevoDiv)
@@ -18,22 +17,28 @@ function pintarTab(numC, numF) {
 		}
 	}
 }
-function generarCampoMinas(numC, numF) {
-	campoMinas = new Array(numF)
-	for (let fila = 0; fila < numF; fila++) {
-		campoMinas[fila] = new Array(numC)
+function generarCampoMinas() {
+	for (let fila = 0; fila < buscaminas.filas; fila++) {
+		campoMinas[fila] = new Array(buscaminas.columnas)
 	}
 }
 function esparcirMinas(numC, numF, minasTotales) {
 	let minasColocadas = 0
 	while (minasColocadas < minasTotales) {
-		let fila = Math.floor(Math.random() * numF)
-		let columna = Math.floor(Math.random() * numC)
+		let fila = Math.floor(Math.random() * (numF))
+		let columna = Math.floor(Math.random() * (numC))
 
-		if (campoMinas[fila][columna] != "B") {
-			campoMinas[fila][columna] = "B"
+		if (campoMinas[fila][columna] != "1") {
+			campoMinas[fila][columna] = "1"
+			document.getElementById(`idCelda_${fila+1}_${columna+1}`).innerHTML = "0"
+			minasColocadas++;
 		}
-		minasColocadas++;
 	}
 	console.log(campoMinas)
+	
+}
+function prueba() {
+	pintarTab(10,10)
+	generarCampoMinas(10,10)
+	esparcirMinas(10,10,50)
 }
